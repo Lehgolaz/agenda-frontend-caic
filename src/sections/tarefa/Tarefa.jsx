ba5349d8ccf971dd97bba699d9c93cb8dea982c6
Lehgolaz <lehgolaz66rodrigues@email.com>
2023-09-11 21:53:24 -0300

    Tarefa.jsx

--- src/sections/tarefa/Tarefa.jsx
+++ src/sections/tarefa/Tarefa.jsx
@@ -1,3 +1,30 @@
-import './Tarefa.css'
-import React, { useEffect, useState } from 'react'
-import api from '../../api/Api'
+import React from "react";
+
+
+const Tarefa = () => {
+    //'data', 'assunto', 'descricao', 'contato', 'tipo_id', 'created_at', 'updated_at'
+    return (
+        <div>
+            <h2>Tarefa</h2>
+            <form action=""></form>
+            <div>
+                <label htmlFor="data">Data</label>
+                <input type="date" id="data" name="data" />
+            </div>
+            <div>
+            <label htmlFor="assunto">Assunto</label>
+                    <input type="text" id="assunto" name="assunto" />
+            </div>
+            <div>
+            <label htmlFor="descricao">Descricao</label>
+                    <textarea name="descricao" id="descricao" cols="30" rows="10"></textarea>
+            </div>
+            <div>
+            <label htmlFor="contato">Contato</label>
+                    <input type="text" id="contato" name="contato" />
+            </div>
+        </div>
+    )
+}
+
+export default Tarefa
